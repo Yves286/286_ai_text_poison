d9deccf569790bd2d0ef5f6c475677bae7599939
Yves286 <140513871+Yves286@users.noreply.github.com>
2025-02-02 21:03:02 +1100

    single string encoded in console

--- code/main.py
+++ code/main.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import random
 from confuseables_dict import confuseables_dict
-import re
 
-string = "BING BONG!! You have been caught by the A.I. police!" # pass a string
-output = ""
-for c in string: # for each character, find the matching key in confuseables_dict
-    if c in confuseables_dict: # IF match - replace with random character in value
-        confuseable_chars = confuseables_dict[c]# for each character, find the matching key in confuseables_dict
-        rand_int = random.randint(0, len(confuseable_chars)-1)
-        output = output + confuseable_chars[rand_int]
-    else:
-        output = output + c
-print(output)
+def replace_char(str_input):
+    output = ""
+    for c in str_input: # for each character, find the matching key in confuseables_dict
+        if c in confuseables_dict: # IF match - replace with random character in value
+            confuseable_chars = confuseables_dict[c]# for each character, find the matching key in confuseables_dict
+            rand_int = random.randint(0, len(confuseable_chars)-1)
+            output = output + confuseable_chars[rand_int]
+        else:
+            output = output + c
+    return output
+        
+
+if __name__ == "__main__":
+    str_user = None
+    print(type(str_user))
+    while (str_user is None) or (len(str_user) == 0):
+        str_user = input("Input the text you want encoded: ")
+    print("Your encoded text is: " + replace_char(str_user))
